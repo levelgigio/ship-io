e35e5c22c9b6053cf4d0de6d1c266667ccdfe418
NEOGIGIO <gigio.forastieri@gmail.com>
2018-08-06 21:26:58 -0300

    add continuous animation

--- Sprite.js
+++ Sprite.js
@@ -3,25 +3,59 @@ function Sprite(json) {
     this.y = 20;
     this.json = jQuery.parseJSON(json);
     this.img_div = document.getElementById(this.json.object_name);
+    this.continuous = false;
     this.current_frame = 0;
     this.current_animation;
+    this.previous_animation;
+    this.animation_interval;
+    
+    //------------------------------------------------ //
+    
     this.animate = function(animation) {
 
         if (this.current_animation !== animation) {
+            this.previous_animation = this.current_animation;
             this.current_animation = animation;
             this.current_frame = 0;
+            clearInterval(this.animation_interval);
         }
-        
-        console.log(this.json.animations[this.current_animation].frames[this.current_frame].file);
-        var object = this;
 
-        var animation_interval = window.setInterval(function() {
+        var object = this;
+        
+        this.animation_interval = setInterval(function() {
             object.img_div.src = object.json.animations[object.current_animation].frames[object.current_frame].file;
             console.log(object.current_frame);
             object.current_frame = (object.current_frame+1)%object.json.animations[object.current_animation].frames.length;
-            if(!object.current_frame)
-                window.clearInterval(animation_interval);
+            if(!object.current_frame) {
+                clearInterval(object.animation_interval);
+                if(object.continuous)
+                    object.continuous_animate(object.previous_animation);
+            }
         }, this.json.animations[this.current_animation].animation_velocity/this.json.animations[this.current_animation].frames.length);
         
     }
+    
+    //------------------------------------------------ //
+    
+    this.continuous_animate = function(animation) {
+        
+        this.continuous = true;
+        
+        if (this.current_animation !== animation) {
+            this.current_animation = animation;
+            this.current_frame = 0;
+            clearInterval(this.animation_interval);
+        }
+
+        var object = this;
+        
+        this.animation_interval = setInterval(function() {
+            object.img_div.src = object.json.animations[object.current_animation].frames[object.current_frame].file;
+            object.current_frame = (object.current_frame+1)%object.json.animations[object.current_animation].frames.length;
+        }, this.json.animations[this.current_animation].animation_velocity/this.json.animations[this.current_animation].frames.length);
+        
+    }
+    
+    //------------------------------------------------ //
+    
 }
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     var horse = new Sprite(horse_json);
+    horse.continuous_animate("horse_run");
     $("#animate-btn").click(function() {
         horse.animate("horse_jump");
     });
